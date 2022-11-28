0940602be2ef4e3503cea0cef6ec886e3d70851d
aulia octavia <auliaoctaviam28@gmail.com>
2022-11-28 20:08:05 +0700

    y

--- app/src/main/java/com/smkn9semarang/quizzz/easy.java
+++ app/src/main/java/com/smkn9semarang/quizzz/easy.java
@@ -1,9 +1,9 @@
 package com.smkn9semarang.quizzz;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.animation.Animation;
@@ -13,23 +13,25 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Random;
-import android.os.Bundle;
 
 public class easy extends AppCompatActivity {
 
     private int presCounter = 0;
-    private int maxPresCounter = 4;
-    private String[] keys = {"R", "I", "B", "D", "X"};
-    private String textAnswer = "BIRD";
+    private int maxPresCounter = 5;
+    private String[] keys = {"J", "M", "A", "N", "E"};
+    private String textAnswer = "JAM";
     TextView textScreen, textQuestion, textTitle;
     Animation smallbigforth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy);
+
+        smallbigforth = AnimationUtils.loadAnimation(this, R.anim.smallbigforth);
 
         keys = shuffleArray(keys);
 
@@ -37,7 +39,7 @@ public class easy extends AppCompatActivity {
             addView(((LinearLayout) findViewById(R.id.layoutParent)), key, ((EditText) findViewById(R.id.editText)));
         }
 
-        maxPresCounter = 4;
+        maxPresCounter = 3;
     }
 
 
@@ -59,17 +61,30 @@ public class easy extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
 
-        linearLayoutParams.rightMargin = 30;
+        linearLayoutParams.rightMargin = 5;
 
         final TextView textView = new TextView(this);
 
         textView.setLayoutParams(linearLayoutParams);
-        textView.setTextColor(this.getResources().getColor(R.color.white));
+        textView.setBackground(this.getResources().getDrawable(R.drawable.bgpink));
+        textView.setTextColor(this.getResources().getColor(R.color.black));
         textView.setGravity(Gravity.CENTER);
         textView.setText(text);
         textView.setClickable(true);
         textView.setFocusable(true);
         textView.setTextSize(32);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (TextView) findViewById(R.id.textTitle);
+
+        textQuestion.setTypeface(typeface);
+        textScreen.setTypeface(typeface);
+        textTitle.setTypeface(typeface);
+        editText.setTypeface(typeface);
+        textView.setTypeface(typeface);
 
         textView.setOnClickListener(new View.OnClickListener() {
 
@@ -112,7 +127,7 @@ public class easy extends AppCompatActivity {
 
             editText.setText("");
         } else {
-            Toast.makeText(easy.this, "Wrong", Toast.LENGTH_SHORT).show();
+            Toast.makeText(easy.this, "jawaban salah", Toast.LENGTH_SHORT).show();
             editText.setText("");
         }
 
@@ -121,5 +136,8 @@ public class easy extends AppCompatActivity {
         for (String key : keys) {
             addView(linearLayout, key, editText);
         }
+
     }
+
+
 }
